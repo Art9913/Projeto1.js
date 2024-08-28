95a65f5399cd9362a9b52492000eec7133f2e904
Arthur de Sousa <arthur.smota@sempreceub.com>
2024-08-27 21:11:59 -0300

    Projeto1.js

---
+++ Projeto1.js
@@ -0,0 +1,5 @@
+var primeiroNome = prompt("insira o nome");
+var ultimo_nome = prompt("insira o ultimo nome");
+var NomeCompleto = primeiroNome + " " + ultimo_nome;
+console.log("Olá, meu nome é " + NomeCompleto);
+console.log(NomeCompleto.slice(0,3));
